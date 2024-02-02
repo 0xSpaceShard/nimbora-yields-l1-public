61089b143c2413077f8152d115b1fe85c80c5195
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2024-02-02 09:17:05 +0100

    refactor: scripts

--- scripts/registerStrategies.ts
+++ scripts/registerStrategies.ts
@@ -1,19 +1,19 @@
-import { ethers } from "hardhat";
-import { networkAddresses } from "./config";
+import { ethers } from 'hardhat';
+import { networkAddresses } from './config';
 
 async function main() {
     const [deployer] = await ethers.getSigners();
     const balance0ETH = await ethers.provider.getBalance(deployer.address);
-    console.log("User Address:", deployer.address);
-    console.log("User Balance:", ethers.formatEther(balance0ETH));
+    console.log('User Address:', deployer.address);
+    console.log('User Balance:', ethers.formatEther(balance0ETH));
     const addresses = networkAddresses['mainnet'];
-    const poolingManager = await ethers.getContractAt("PoolingManager", addresses.l1PoolingManager);
+    const poolingManager = await ethers.getContractAt('PoolingManager', addresses.l1PoolingManager);
     try {
-        console.log("Registering Uni strategies");
-        await (poolingManager).registerStrategy(addresses.uniStrategy, addresses.weth, addresses.ethBridge);
-        await (poolingManager).registerStrategy(addresses.sdaiStrategy, addresses.dai, addresses.daiBridge);
+        console.log('Registering Uni strategies');
+        await poolingManager.registerStrategy(addresses.uniStrategy, addresses.weth, addresses.ethBridge);
+        await poolingManager.registerStrategy(addresses.sdaiStrategy, addresses.dai, addresses.daiBridge);
     } catch (error) {
-        console.error("Error:", error);
+        console.error('Error:', error);
     }
 }
 
@@ -21,5 +21,3 @@ main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
-
-
