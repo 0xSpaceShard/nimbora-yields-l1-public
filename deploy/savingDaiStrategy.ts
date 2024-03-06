330c442cc354ea77d031fe4ed1f1c2101d83c671
0xSacha <sacha.graff@icloud.com>
2024-03-06 03:16:56 +0100

    e

--- deploy/savingDaiStrategy.ts
+++ deploy/savingDaiStrategy.ts
@@ -8,24 +8,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deploy, get } = deployments;
     const network: string = hre.network.name;
     const addresses = network == 'mainnet' ? networkAddresses['mainnet'] : networkAddresses['goerli'];
-    const savingDaiStrategyDeployment = await deploy('SavingDaiStrategy', {
-        from: deployer,
-        log: true,
-        contract: 'SavingDaiStrategy',
-        proxy: {
-            proxyContract: 'OpenZeppelinTransparentProxy',
-            execute: {
-                init: {
-                    methodName: 'initialize',
-                    args: [
-                        addresses.l1PoolingManager, addresses.dai, addresses.sdai, addresses.daiBridge
-                    ],
-                },
-            },
-        },
-    });
+    // const savingDaiStrategyDeployment = await deploy('SavingDaiStrategy', {
+    //     from: deployer,
+    //     log: true,
+    //     contract: 'SavingDaiStrategy',
+    //     proxy: {
+    //         proxyContract: 'OpenZeppelinTransparentProxy',
+    //         execute: {
+    //             init: {
+    //                 methodName: 'initialize',
+    //                 args: [
+    //                     addresses.l1PoolingManager, addresses.dai, addresses.sdai, addresses.daiBridge
+    //                 ],
+    //             },
+    //         },
+    //     },
+    // });
 
-    console.log(`SavingDai strategy deployed at ${savingDaiStrategyDeployment.address}`);
+    // console.log(`SavingDai strategy deployed at ${savingDaiStrategyDeployment.address}`);
 };
 
 export default func;
--- deploy/starknetPoolingManager.ts
+++ deploy/starknetPoolingManager.ts
@@ -11,26 +11,31 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const addresses = network == 'mainnet' ? networkAddresses['mainnet'] : networkAddresses['goerli'];
 
     const balance = await hre.ethers.provider.getBalance(deployer);
-    console.log("deployer", deployer)
-    console.log("balance", balance)
+    console.log('deployer', deployer);
+    console.log('balance', balance);
 
-    // const starknetPoolingManagerDeployment = await deploy('StarknetPoolingManager', {
-    //     from: deployer,
-    //     log: true,
-    //     contract: 'StarknetPoolingManager',
-    //     proxy: {
-    //         proxyContract: 'OpenZeppelinTransparentProxy',
-    //         execute: {
-    //             init: {
-    //                 methodName: 'initialize',
-    //                 args: [
-    //                     deployer, addresses.l2PoolingManager, addresses.starknetCore, addresses.relayer, addresses.ethBridge, addresses.weth
-    //                 ],
-    //             },
-    //         },
-    //     },
-    // });
-    // console.log(`Pooling manager contract deployed at ${starknetPoolingManagerDeployment.address}`);
+    const starknetPoolingManagerDeployment = await deploy('StarknetPoolingManager', {
+        from: deployer,
+        log: true,
+        contract: 'StarknetPoolingManager',
+        proxy: {
+            proxyContract: 'OpenZeppelinTransparentProxy',
+            execute: {
+                init: {
+                    methodName: 'initialize',
+                    args: [
+                        deployer,
+                        addresses.l2PoolingManager,
+                        addresses.starknetCore,
+                        addresses.relayer,
+                        addresses.ethBridge,
+                        addresses.weth,
+                    ],
+                },
+            },
+        },
+    });
+    console.log(`Pooling manager contract deployed at ${starknetPoolingManagerDeployment.address}`);
 };
 
 export default func;
